d357b4dffb28c511c1d543c3f97a2fce104ed618
Eder <edermitten@gmail.com>
2018-09-25 17:18:00 -0500

    Initial Commit

---
+++ Memorable Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memorable Places
+//
+//  Created by Eder Mitten on 9/25/18.
+//  Copyright © 2018 Eder Mitten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
